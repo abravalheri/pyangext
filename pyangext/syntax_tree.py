53445ce8c3c6a53f8eb3100e616c96cbcb4428f2
Anderson Bavalheri <andersonbravalheri@gmail.com>
2016-05-17 20:09:48 -0300

    feat(builder): change builder to use wrapper
    Wrapper allows a more direct manipulation of tree, including:
    - 2 styles of building trees: nested X procedural
    - auto append for procedural style
    - direct dump
    
    Commit includes unit tests

--- pyangext/syntax_tree.py
+++ pyangext/syntax_tree.py
@@ -4,7 +4,8 @@ from collections import namedtuple
 
 from six import StringIO
 
-from pyang.statements import Statement
+from pyang import statements as st
+from pyang.error import Position
 from pyang.translators import yang
 
 from pyangext import __version__  # noqa
@@ -23,6 +24,11 @@ _ctx = _SimpleContext(  # pylint: disable=invalid-name
     _YangOptions(False, True))
 
 
+class ValidationError(RuntimeError):
+    """Validation not alowed"""
+    pass
+
+
 class YangBuilder(object):
     """YANG Statement generator factory.
 
@@ -33,16 +39,17 @@ class YangBuilder(object):
     method is used as keyword for the statement. Underscore are replaced
     by dashes and duble underscores denote separation of a prefix::
 
-        >>> from pyangext.syntax_tree import builder as Y
-        >>> Y.dump(Y.leaf_list('allow-user', [
+        >>> from pyangext.syntax_tree import YangBuilder
+        >>> Y = YangBuilder('desired-module-name')
+        >>> Y.leaf_list('allow-user', [
                 Y.type('string'), Y.description('username')
-            ]))
+            ]).dump()
         # => 'leaf-list allow-user {
         #       type string;
         #       description "username";
         #     }'
-        >>> Y.dump(Y.ext__c_define(
-                'INTERFACES', Y.if_feature('local-storage')))
+        >>> Y.ext__c_define(
+                'INTERFACES', Y.if_feature('local-storage')).dump()
         # => 'ext:c-define "INTERFACES" {
         #       if-feature local-storage;
         #     }'
@@ -51,14 +58,44 @@ class YangBuilder(object):
     keyword for the statement and the second is used as its argument.
     Optional children and parent nodes can be passed::
 
-        >>> Y.dump(Y('ext:c-define', 'INTERFACES',
-                     Y.if_feature('local-storage')))
+        >>> Y('ext:c-define', 'INTERFACES',
+                     Y.if_feature('local-storage')).dump()
         # => 'ext:c-define "INTERFACES" {
         #       if-feature local-storage;
         #     }'
 
+    Note that Builder is stateful, it means it cannot be shared among
+    threads or even used to build multiple trees simultaneously.
+
+    The prefered way of building ASTs is by passing children as argument,
+    but is also possible to delay the children creation::
+
+        >>> tl = Y.leaf_list('text-lines')
+        >>> tl.type('string')
+        >>> tl.description('lines of a text')
+        >>> tl.dump()
+        # => 'leaf-list text-lines {
+        #       type string;
+        #       description "lines of a text";
+        #     }'
+
     """
-    # pylint: disable=no-self-use
+    def __init__(self, name='builder-generated', top=None, keyword='module'):
+        """Initialize builder.
+
+        Arguments:
+            name (str): desired name for a hypothetical output module.
+            top (pyang.statements.Statement): outer-most statement
+                (useful for sub-trees)
+        """
+        self._pos = (top and top.pos) or Position(name)
+
+        # Creates a dummy outermost module statement to simplify
+        # traversing tree logic
+        self._top = top or st.Statement(None, None, self._pos, 'module', name)
+
+        if not self._pos.top:
+            self._pos.top = self._top
 
     def __call__(self, keyword, arg=None, children=None, parent=None):
         """Magic method to generate YANG statements.
@@ -76,35 +113,54 @@ class YangBuilder(object):
         """
         children = children or []
 
-        if hasattr(arg, '__iter__') or isinstance(arg, Statement):
+        if (hasattr(arg, '__iter__') or
+                isinstance(arg, (st.Statement, StatementWrapper))):
             children = arg
             arg = None
 
-        if not hasattr(children, '__iter__'):
+        if not (hasattr(children, '__iter__') and children.__iter__):
             children = [children]
 
-        node = Statement(None, parent, None, keyword, arg)
-        node.substmts = children
+        if keyword in ('module', 'submodule'):
+            node = self._top
+            node.keyword = keyword
+            node.arg = arg
+            node.i_module = node
+        else:
+            node = st.Statement(
+                self._top, parent, self._top.pos, keyword, arg)
+            node.i_module = self._top
 
-        return node
+        unwraped_children = []
+        for child in children:
+            if isinstance(child, StatementWrapper):
+                unwraped = child._statement
+            else:
+                unwraped = child
+            unwraped.parent = node
+            unwraped_children.append(unwraped)
+
+        node.substmts = unwraped_children
+
+        return StatementWrapper(node, self)
 
     def __getattr__(self, keyword):
         """Magic method to generate YANG statements."""
         keyword = keyword.replace('__', ':').replace('_', '-')
+        build = self.__call__
 
         def _factory(arg=None, children=None, prefix=None, **kwargs):
             node_type = keyword
             if prefix is not None:
                 node_type = PREFIX_SEPARATOR.join([prefix, keyword])
 
-            return self.__call__(node_type, arg, children, **kwargs)
+            return build(node_type, arg, children, **kwargs)
 
         return _factory
 
     def blankline(self):
         """Insert a empty line."""
-        return Statement(
-            None, None, None, '_comment', '')
+        return self.__call__('_comment', '')
 
     def comment(self, text):
         """Generate a comment node.
@@ -114,12 +170,15 @@ class YangBuilder(object):
         """
         lines = text.strip().splitlines()
         if len(lines) == 1:
-            return Statement(
-                None, None, None, '_comment', '// ' + lines[0])
+            text = '// ' + lines[0]
+        else:
+            text = (
+                '/*\n' +
+                '\n'.join(['* ' + line for line in lines]) +
+                '\n*/'
+            )
 
-        lines = '\n'.join(['* ' + line for line in lines])
-        return Statement(
-            None, None, None, '_comment', '/*\n' + lines + '\n*/')
+        return self.__call__('_comment', text)
 
     def dump(
             self, node, file_obj=None,
@@ -145,6 +204,9 @@ class YangBuilder(object):
         # create a buffer to allow string return if no file_obj given
         _file_obj = file_obj or StringIO()
 
+        if isinstance(node, StatementWrapper):
+            node = node._statement
+
         # process AST
         yang.emit_stmt(
             ctx, node, _file_obj, 1, None, prev_indent, indent_string)
@@ -152,5 +214,87 @@ class YangBuilder(object):
         # oneliners <3: if no file_obj get buffer content and close it!
         return file_obj or (_file_obj.getvalue(), _file_obj.close())[0]
 
-builder = YangBuilder()  # pylint: disable=invalid-name
-"""Element factory object."""
+
+class StatementWrapper(object):
+    def __init__(self, statement, builder):
+        """Create a builder wrapper around ``pyang.statements.Statement``.
+
+        This wrapper can be used to generate substatements, or dump the
+        syntax tree as a YANG string representation.
+
+        Example:
+        ::
+            >>> from pyangext.syntax_tree import YangBuilder
+            >>> Y = YangBuilder('desired-module-name')
+            >>> wrapper = Y.leaf_list('allow-user')
+            >>> wrapper.type('string')
+            >>> wrapper.dump()
+            # => 'leaf-list allow-user {
+            #       type string;
+            #     }'
+        """
+        self._statement = statement
+        self._builder = builder
+        self.__iter__ = None
+
+    def __call__(self, *args, **kwargs):
+        """Call ``__call__`` from builder, adding result as substatement.
+
+        See :meth:`YangBuilder.__call__`.
+        """
+        kwargs.setdefault('parent', self._statement)
+        other_wrapper = self._builder.__call__(*args, **kwargs)
+        self._statement.substmts.append(other_wrapper._statement)
+
+        return other_wrapper
+
+    def __getattr__(self, name):
+        """Call ``__getattr__`` from builder, adding result as substatement.
+
+        See :meth:`YangBuilder.__getattr__`.
+        """
+        method = self._builder.__getattr__(name)
+        parent = self._statement
+
+        def _call(*args, **kwargs):
+            kwargs.setdefault('parent', self._statement)
+            other_wrapper = method(*args, **kwargs)
+            parent.substmts.append(other_wrapper._statement)
+
+            return other_wrapper
+
+        return _call
+
+    def dump(self, *args, **kwargs):
+        """Returns the string representation of the YANG module.
+
+        See :meth:`YangBuilder.dump`.
+        """
+        return self._builder.dump(self._statement, *args, **kwargs)
+
+    def unwrap(self):
+        """Retrieve the inner ``pyang.statements.Statement`` object"""
+        return self._statement
+
+    def validate(self, ctx):
+        """Validates the syntax tree.
+
+        Should be called just from ``module``, ``submodule`` statements.
+
+        Arguments:
+            ctx (pyang.Context): object generated from pyang parsing
+        """
+        node = self._statement
+        if node.keyword not in ('module', 'submodule'):
+            raise ValidationError(
+                'Cannot validate `%d`, only top-level statements '
+                '(module, submodule)', node.keyword)
+
+        return st.validate_module(ctx, node)
+
+    def __repr__(self):
+        """Unique representation for debugging purposes."""
+        node = self._statement
+        return '<{}.{}({} "{}") at {}>'.format(
+            self.__module__, type(self).__name__,
+            node.keyword, node.arg, hex(id(self)))
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# pylint: disable=redefined-outer-name
 """
 tests for YANG builder
 """
+import pytest
+from mock import Mock
+
 from pyang.statements import Statement
 
-from pyangext.syntax_tree import builder
+from pyangext.syntax_tree import ValidationError, YangBuilder
 
 __author__ = "Anderson Bravalheri"
 __copyright__ = "Copyright (C) 2016 Anderson Bravalheri"
 __license__ = "mozilla"
 
 
-def test_dump():
+@pytest.fixture
+def builder():
+    """Default YANG AST builder"""
+    return YangBuilder()
+
+
+def test_dump(builder):
     """
     dump should correctly print headless pyang.statements.Statement
     dump should correctly print nested pyang.statements.Statement
@@ -30,7 +40,7 @@ def test_dump():
     )
 
 
-def test_call():
+def test_call(builder):
     """
     calling build should build statements
     calling build directly with children should build nested statements
@@ -53,7 +63,7 @@ def test_call():
     )
 
 
-def test_getattr():
+def test_getattr(builder):
     """
     calling undefined method build should build statements
     double underscores should be transformed into prefix
@@ -70,7 +80,7 @@ def test_getattr():
     assert builder.dump(extension).strip() == 'ext:c-define "INTERFACES";'
 
 
-def test_comment():
+def test_comment(builder):
     """
     single line comments should start with double slashs
     """
@@ -86,9 +96,142 @@ def test_comment():
     )
 
 
-def test_blankline():
+def test_blankline(builder):
     """
     blank lines should be empty
     """
     blankline = builder.blankline()
     assert builder.dump(blankline).strip() == ''
+
+
+def test_wrapper_dump(builder):
+    """
+    dump should correctly print wrapper
+    wrapper should dump itself
+    """
+    module = builder.module('test', [
+        builder.prefix('test'),
+        builder.namespace('urn:yang:test')
+    ])
+
+    assert builder.dump(module).strip() == (
+        'module test {\n'
+        '  namespace "urn:yang:test";\n'
+        '  prefix test;\n'
+        '}'
+    )
+
+    assert module.dump().strip() == (
+        'module test {\n'
+        '  namespace "urn:yang:test";\n'
+        '  prefix test;\n'
+        '}'
+    )
+
+
+def test_wrapper_attribute(builder):
+    """
+    wrapper should allow direct attribute
+    """
+    module = builder.module('test')
+    module.prefix('test')
+    module.namespace('urn:yang:test')
+
+    assert module.dump().strip() == (
+        'module test {\n'
+        '  namespace "urn:yang:test";\n'
+        '  prefix test;\n'
+        '}'
+    )
+
+
+def test_wrapper_call(builder):
+    """
+    wrapper should allow direct call
+    """
+    module = builder('module', 'test')
+    module('prefix', 'test')
+    module('namespace', 'urn:yang:test')
+
+    assert module.dump().strip() == (
+        'module test {\n'
+        '  namespace "urn:yang:test";\n'
+        '  prefix test;\n'
+        '}'
+    )
+
+
+def test_mix_builder(builder):
+    """
+    builder should mix with pyang standard
+    """
+    module = builder(
+        'module', 'test',
+        Statement(None, None, None, 'namespace', 'urn:yang:test')
+    )
+    module('prefix', 'test')
+
+    assert module.dump().strip() == (
+        'module test {\n'
+        '  namespace "urn:yang:test";\n'
+        '  prefix test;\n'
+        '}'
+    )
+
+
+def test_statement_without_arg(builder):
+    """
+    builder should allow bypassing ``arg``  as positional argument,
+        in other words, pass ``children`` after ``keyword``
+    """
+    module = builder('module', 'test', [
+        builder.namespace('urn:yang:test'),
+        builder.prefix('test'),
+        builder.rpc('perform', builder.input(
+            builder.leaf('name', builder.type('string'))
+        )),
+        builder.rpc('eval', builder(
+            'input',
+            builder.leaf('name', builder.type('string'))
+        )),
+    ])
+
+    assert module.dump().strip() == (
+        'module test {\n'
+        '  namespace "urn:yang:test";\n'
+        '  prefix test;\n'
+        '  rpc perform {\n'
+        '    input {\n'
+        '      leaf name {\n'
+        '        type string;\n'
+        '      }\n'
+        '    }\n'
+        '  }\n'
+        '  rpc eval {\n'
+        '    input {\n'
+        '      leaf name {\n'
+        '        type string;\n'
+        '      }\n'
+        '    }\n'
+        '  }\n'
+        '}'
+    )
+
+
+def test_builder_unwrap(builder):
+    """
+    unwrap should return pyang.statements.Statement
+    """
+    module = builder('module', 'test')
+    assert isinstance(module.unwrap(), Statement)
+
+
+def test_wrapper_validate(builder):
+    """
+    validate should not allow non top-level statements
+    """
+    leaf = builder.leaf('name', builder.type('string'))
+    ctx = Mock()
+
+    with pytest.raises(ValidationError):
+        leaf.validate(ctx)
